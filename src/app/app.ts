041d26ed96f4f751854e8bad435a6d2c65df67b9
cequella <pedro.oliveira.demolay@gmail.com>
2026-02-02 01:55:53 -0300

    Adding favicon ang page title

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,4 @@
-import { Component, signal, inject } from '@angular/core';
+import { Component, signal, inject, effect } from '@angular/core';
 import { RouterOutlet, RouterLink } from '@angular/router';
 import { ThemeService } from './theme.service';
 import { LanguageService } from './language.service';
@@ -12,5 +12,15 @@ import { LanguageService } from './language.service';
 export class App {
   protected readonly themeService = inject(ThemeService);
   protected readonly langService = inject(LanguageService);
-  protected readonly title = signal('art-portfolio');
+  protected readonly title = signal('D.Çeqüella - Programação é Arte!!!');
+
+  constructor() {
+    effect(() => {
+      const isPt = this.langService.lang() === 'pt';
+      const newTitle = isPt
+        ? 'D.Çeqüella - Programação é Arte!!!'
+        : 'D.Çeqüella - Coding is Art!!!';
+      document.title = newTitle;
+    });
+  }
 }
